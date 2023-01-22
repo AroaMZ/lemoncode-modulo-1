a6d4b137741bca6b34d122a9ba67089e0eae0014
Aroa Mendez <aroazmendez@gmail.com>
2023-01-22 19:52:11 +0100

    Modulo 1

---
+++ js/demo.js
@@ -0,0 +1,3 @@
+document.getElementById("nombre").value = "Aroa"
+document.getElementById("apellido").value = "Mendez"
+document.getElementById("imagen").innerHTML='<img src="https://cdn-icons-png.flaticon.com/512/3577/3577429.png" />'
